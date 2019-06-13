391db775e05e2a4e15b263957333a4992395495f
Фарида <violence8888@gmail.com>
2019-06-13 15:11:08 +0500

    Добавьте файлы проекта.

---
+++ Models/Hospital.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindHosp.Models
+{
+    class Hospital
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public string Address { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+    }
+}
---
+++ Models/HospitalAddressRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindHosp.Models
+{
+    class HospitalAddressRequest
+    {
+        public string StreetId { get; set; }
+        public string HouseNumber { get; set; }
+    }
+}
